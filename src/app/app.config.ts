9ed3a90fca7cd73d5e0db958d633fe14293bd677
tala salim <210779523+TALA-SALIM@users.noreply.github.com>
2026-05-14 03:04:06 +0300

    fix base href

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideRouter, withHashLocation } from '@angular/router';
+
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(),
+    provideRouter(routes, withHashLocation()), provideClientHydration(withEventReplay()), provideHttpClient(),
   provideHttpClient(withFetch())
   ]
 };
